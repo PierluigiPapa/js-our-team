0f8c2e7e51fff2e338a4716137d2ef33101e438f
PierluigiPapa <pierluigipapa9@gmail.com>
2024-01-19 21:14:09 +0100

    feat: add bonus style

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,27 +37,44 @@ const teamMembri = [
     },
 ]
 
+const divContainer = document.querySelector(".staff");
+
 // console.log(teamMembri);
 
-//Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto 
-for (let i = 0; i < teamMembri.length; i++) {
-    let membri = teamMembri [i]
-    console.log("Nome: " + membri.nome + ", Ruolo: " + membri.ruolo + ", Foto: " + membri.foto);
+//Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+function stampaTeam(teamMembri) {
+    for (let i = 0; i < teamMembri.length; i++) {
+        console.log(`Membro ${i + 1}`)
+        for (let key in teamMembri[i]) {
+            console.log(`${key}: ${teamMembri[i][key]}`);
+        }
+    }
 }
 
-//Stampare le stesse informazioni su DOM sottoforma di stringhe
-let teamContainer = document.getElementById("team-container");
+//Stampare le stesse informazioni su DOM sottoforma di stringhe, per poi inserire il tutto, tramite ciclo, nell'html
+function stampaTeamDom(teamMembri) {
+    const fragment = document.createDocumentFragment();
+    for (let i = 0; i < teamMembri.length; i++) {
+        const divElement = document.createElement("li");
+        divElement.classList.add("team-member");
+        for (let key in teamMembri[i]) {
+            if (key !== "foto") {
+                const divInfo = document.createElement("div");
+                divInfo.classList.add(key);
+                divInfo.append(`${teamMembri[i][key]}`);
+                divElement.append(divInfo);
+            } else {
+                const fotoDiv = document.createElement("img");
+                fotoDiv.classList.add(key);
+                fotoDiv.src = `./assets/img/${teamMembri[i][key]}`;
+                divElement.append(fotoDiv);
+            }
+        }
 
-for (let i = 0; i < teamMembri.length; i++) {
-  let membri = teamMembri[i];
-
- 
-  let memberiInfo = document.createElement("p");
-
-  
-  memberiInfo.textContent = "Nome: " + membri.nome + ", Ruolo: " + membri.ruolo + ", Foto: " + membri.foto;
-
-  
-  teamContainer.appendChild(memberiInfo);
+        fragment.append(divElement);
+    }
+    divContainer.append(fragment);
 }
 
+console.log(stampaTeam(teamMembri));
+stampaTeamDom(teamMembri);
